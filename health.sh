76ccb20f612c97ad37e6e45bc7ac49c8084fa2d4
yuudj <yuudj@hotmail.com>
2022-08-02 13:58:15 -0300

    health script correction

--- health.sh
+++ health.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # DATABASE HEALTH QUERY
-/opt/mssql-tools/bin/sqlcmd -P ${MSSQL_PASSWORD} -S db -U ${MSSQL_USER}  -i /tmp/health.sql
+/opt/mssql-tools/bin/sqlcmd -P ${MSSQL_PASSWORD} -S ${MSSQL_SERVER} -U ${MSSQL_USER} -d ${MSSQL_DATABASE}  -i /tmp/health.sql
